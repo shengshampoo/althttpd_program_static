90d8a900318abb989de07af33c9fcff27aaf27dd
shengshampoo <shengzh@tutanota.com>
2025-09-27 16:59:51 +0000

    Update build-static-althttpd.sh

--- build-static-althttpd.sh
+++ build-static-althttpd.sh
@@ -9,7 +9,7 @@ mkdir -p /work/artifact
 
 # althttpd
 cd $WORKSPACE
-aa=$(curl -sL https://sqlite.org/althttpd/info/tip | grep Check-in | rg -Po 'Check-in\ \[\K[0-9a-z]+' | head -n 1 )
+aa=$(curl -sL https://sqlite.org/althttpd/info/tip | rg Check-in | rg -Po 'Check-in\ \[\K[0-9a-z]+' | head -n 1 )
 curl -sL https://sqlite.org/althttpd/tarball/$aa/althttpd-$aa.tar.gz | tar xv --gzip
 cd althttpd-$aa
 make static-althttpsd
